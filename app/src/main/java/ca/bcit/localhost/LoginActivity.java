b56d5be95e169e44cd8f32a9781f11d03880cb67
Sepehr <sepehrman@icloud.com>
2021-08-29 21:02:41 -0700

    Added user Authentication

--- app/src/main/java/ca/bcit/localhost/LoginActivity.java
+++ app/src/main/java/ca/bcit/localhost/LoginActivity.java
@@ -1,18 +1,29 @@
 package ca.bcit.localhost;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.w3c.dom.Text;
 
 public class LoginActivity extends AppCompatActivity {
 
     Button loginBtn;
-    EditText emailInput, passwordInput;
+    EditText emailEdit, passwordEdit;
+    FirebaseAuth fAuth;
 
 
     @Override
@@ -24,24 +35,51 @@ public class LoginActivity extends AppCompatActivity {
 
         loginBtn = findViewById(R.id.loginUser);
 
-        emailInput = findViewById(R.id.email_input);
-        passwordInput = findViewById(R.id.pass_input);
-
+        emailEdit = findViewById(R.id.email_input);
+        passwordEdit = findViewById(R.id.pass_input);
+        fAuth = FirebaseAuth.getInstance();
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+
+                String emailInput = emailEdit.getText().toString().trim();
+                String passwordInput = passwordEdit.getText().toString().trim();
+
+
+
+                if (TextUtils.isEmpty(emailInput)) {
+                    emailEdit.setError("Please provide an Email Address");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(passwordInput)) {
+                    passwordEdit.setError("Please provide a corresponding password to your Email");
+                    return;
+                }
+
+                if (passwordInput.length() <= 8) {
+                    passwordEdit.setError("Password should be at least 8 characters long");
+                    return;
+                }
+
+
+                fAuth.signInWithEmailAndPassword(emailInput, passwordInput).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(LoginActivity.this, "User Logged In!", Toast.LENGTH_LONG).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        } else {
+                            Toast.makeText(LoginActivity.this, "ERROR: " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+//                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
             }
         });
-
-
-
     }
-
-
-
     public void moveToRegister(View v) {
         startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
     }
